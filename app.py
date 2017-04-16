937cad718e74bbc018205c58ed376964b5bc8250
keijinamba <keiji_1110@yahoo.co.jp>
2017-04-17 00:40:50 +0900

    fix bugs

--- app.py
+++ app.py
@@ -10,12 +10,12 @@ analyze = Analyze()
 @app.route("/", methods = ['GET', 'POST'])
 def index():
 
-	json = request.json
-	app.logger.debug(json)
-	if (json is None or "records" not in json):
+	api_input = json.loads(request.json)
+	app.logger.debug(api_input)
+	if (api_input is None or "records" not in api_input):
 		return jsonify(error = "Need json includes image property which is 784(28 * 28) length, float([0, 1.0]) array")
 	else:
-		result = analyze.emotion(json["records"])
+		result = analyze.emotion(api_input["records"])
 		return jsonify(emotions = result)
 
 if __name__ == "__main__":
